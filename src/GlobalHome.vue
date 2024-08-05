2c28352d548ab5ede1a1cb3b0c107838a063c5fc
cheswin25 <lopezgallegosedwinjose@gmail.com>
2024-08-05 16:56:12 -0600

    lala

--- src/GlobalHome.vue
+++ src/GlobalHome.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app id="inspire" :class="{ 'dark-theme': isDarkMode }" style="height: 202vh; width: 100vw;">
+  <v-app id="inspire" :class="{ 'dark-theme': isDarkMode }" class="app-container">
     <!-- Encabezado -->
     <header>
       <HeaderComponent :title="'HardwareSolutions'" :menuItems="menuItems" />
@@ -25,51 +25,16 @@
             <v-col cols="12" md="6" class="info-col">
               <!-- Información del negocio -->
               <v-sheet class="info-box" elevation="2">
-                <p>
-  <strong>CONÓCENOS</strong><br />
-  En HardwareSolutions, ofrecemos un servicio integral para la reparación de <strong>teléfonos móviles</strong>, <strong>computadoras y laptops</strong>, <strong>electrodomésticos</strong>, y <strong>televisores</strong>. Diagnósticos rápidos y soluciones efectivas para problemas como pantallas rotas, baterías defectuosas, fallos de hardware y software, y más.<br />
-  <strong>Servicio Rápido y Eficiente:</strong> Garantizamos tiempos de reparación cortos para que vuelvas a utilizar tus dispositivos sin demoras.<br />
-  <strong>Soporte Técnico:</strong> Brindamos asesoramiento y asistencia técnica para resolver cualquier duda que puedas tener.
-</p>
+                <p class="info-text">
+                  <strong>CONÓCENOS</strong><br />
+                  En HardwareSolutions, ofrecemos un servicio integral para la reparación de <strong>teléfonos móviles</strong>, <strong>computadoras y laptops</strong>, <strong>electrodomésticos</strong>, y <strong>televisores</strong>. Diagnósticos rápidos y soluciones efectivas para problemas como pantallas rotas, baterías defectuosas, fallos de hardware y software, y más.<br />
+                  <strong>Servicio Rápido y Eficiente:</strong> Garantizamos tiempos de reparación cortos para que vuelvas a utilizar tus dispositivos sin demoras.<br />
+                  <strong>Soporte Técnico:</strong> Brindamos asesoramiento y asistencia técnica para resolver cualquier duda que puedas tener.
+                </p>
               </v-sheet>
             </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-    </main>
-    <!-- Pie de página -->
-    <footer style="max-height: 750px; overflow: hidden;">
-      <div class="container">
-        <v-row align="center" justify="start">
-          <v-col cols="12" md="8" class="text-center">
-            <br />
-            <v-row
-              style="position: absolute; top: 1230px; left: 40px; color: aliceblue"
-              class="social-icons">
-              <v-btn
-                icon
-                class="mx-2"
-                href="https://www.facebook.com/profile.php?id=100054380206513"
-                target="_blank"
-              >
-                <font-awesome-icon :icon="['fab', 'facebook']" />
-                <v-img class="icon" :src="logo" contain></v-img>
-              </v-btn>
-              <v-btn icon class="mx-2" href="https://whatsapp.com" target="_blank">
-                <v-icon size="30" class="white--text">mi-whatsapp></v-icon>
-              </v-btn>
-              <v-btn icon class="mx-2" href="https://instagram.com" target="_blank">
-                <v-icon size="30" class="white--text">mdi-instagram</v-icon>
-              </v-btn>
-            </v-row>
-
-            <h5 class="my-4 white--text">
-              <h3
-                style="position: absolute; top: 780px; left: 100px; color: #ffad00; font-size: 110px;">
-                Hardware
-                <br/>Solutions
-              </h3>
-              <!---MAPA-->
+            <v-col cols="12" md="6">
+              <!-- Mapa -->
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3600.655566511074!2d-103.41784522489935!3d25.51653121869297!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x868fdb87c9af3bab%3A0x48699817edaff153!2sLa%20Fuente!5e0!3m2!1ses-419!2smx!4v1722614399479!5m2!1ses-419!2smx"
                 width="100%"
@@ -78,90 +43,44 @@
                 loading="lazy"
                 referrerpolicy="no-referrer-when-downgrade"
               ></iframe>
-              <v-divider
-  color="#E8EAF6"
-  style="
-    width: 170vw;
-    position: relative;
-    left: 50%;
-    transform: translateX(-50%) translateY(60px); /* Agregué un desplazamiento vertical de 20px */
-  "
-></v-divider>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+    </main>
 
+    <!-- Pie de página -->
+    <footer>
+      <v-container>
+        <v-row>
+          <!-- Información de Contacto -->
+          <v-col cols="12" md="4" class="footer-info">
+            <h4 class="footer-title">Contactos</h4>
+            <p>MundoElectronicoTRC@gmail.com</p>
+            <p>Teléfono: 8715265042</p>
+          </v-col>
 
-              <v-btn
-                style="position: absolute; top: 1297px; left: 400px; color: #ffad00"
-                class="transparent-btn"
-              >
-                Inicio
-              </v-btn>
+          <!-- Enlaces de Navegación -->
+          <v-col cols="12" md="4" class="footer-links">
+            <h4 class="footer-title">Desarrolladores</h4>
+            <p>Edwin Lopez, Carlos Centeno</p>
+            <p>Diana Ochoa, Marbella Perez</p>
+          </v-col>
 
-              <v-btn
-                style="position: absolute; top: 1297px; left: 640px; color: #ffad00"
-                class="transparent-btn"
-                >Login</v-btn
-              >
-
-              <v-btn
-                style="position: absolute; top: 1297px; left: 800px; color: #ffad00"
-                class="transparent-btn"
-                >Iniciar Secion</v-btn
-              >
-              <h3 style="position: absolute; top: 1297px; left: 40px; color: aliceblue">
-                Telefono:8715265042
-              </h3>
-              <br />
-              <br />
-              <h3 style="position: absolute; top:  1320px; left: 40px; color: aliceblue">
-                MundoElectronicoTRC@gmail.com
-              </h3>
-              <br />
-              <v-row style="position: absolute; top:  1297px; left: 1020px; color: #ffad00">
-                <v-col cols="auto">
-                  <h3 style="color: #ffad00">Desarrolladores</h3>
-                </v-col>
-              </v-row>
-              <v-row
-                style="
-                  position: absolute;
-                  top: 1297px;
-                  left: 1020px;
-                  color: #ffad00;
-                  font-size: 14px;
-                "
-              >
-                <v-col cols="auto">
-                  <h3 style="color: aliceblue">Carlos Centeno</h3>
-                </v-col>
-              </v-row>
-              <v-row style="position: absolute; top: 1320px; left: 1020px; color: #ffad00">
-                <v-col cols="auto">
-                  <h3 style="color: aliceblue">Diana Ochoa</h3>
-                </v-col>
-              </v-row>
-              <v-row style="position: absolute; top: 1340px; left: 1020px; color: #ffad00">
-                <v-col cols="auto">
-                  <h3 style="color: aliceblue">Marbella Perez</h3>
-                </v-col>
-              </v-row>
-              <v-row style="position: absolute; top: 1360px; left: 1020px; color: #ffad00">
-                <v-col cols="auto">
-                  <h3 style="color: aliceblue">Edwin Lopez</h3>
-                </v-col>
-              </v-row>
-            </h5>
-            <v-row style="position: absolute; top: 1297px; left: 1290px; color: #ffad00">
-              <v-col cols="auto">
-                <h4 style="color: #ffad00">Direccion</h4>
-              </v-col>
-            </v-row>
-            <v-row style="position: absolute; top: 1300px; left: 1190px; color: #ffad00">
-              <v-col cols="auto">
-                <h4 style="color: aliceblue">
-                  Calle dia de la fuente No.1463 6 campestre la rosita , Torreón, Mexico
-                </h4>
-              </v-col>
-            </v-row>
+          <!-- Iconos Sociales -->
+          <v-col cols="12" md="4" class="social-icons">
+            <v-btn icon href="https://www.facebook.com/profile.php?id=100054380206513" target="_blank" class="social-icon-btn">
+              <!-- Icono de Facebook -->
+              <svg fill="#000000" width="24px" height="24px" viewBox="0 0 24 24" id="facebook" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" class="icon flat-color">
+                <path d="M14,6h3a1,1,0,0,0,1-1V3a1,1,0,0,0-1-1H14A5,5,0,0,0,9,7v3H7a1,1,0,0,0-1,1v2a1,1,0,0,0,1,1H9v7a1,1,0,0,0,1,1h2a1,1,0,0,0,1-1V14h2.22a1,1,0,0,0,1-.76l.5-2a1,1,0,0,0-1-1.24H13V7A1,1,0,0,1,14,6Z" style="fill: rgb(0, 0, 0);"></path>
+              </svg>
+            </v-btn>
+            <v-btn icon href="https://instagram.com" target="_blank" class="social-icon-btn">
+              <!-- Icono de Instagram -->
+              <svg fill="#000000" width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1">
+                <path d="M17.34,5.46h0a1.2,1.2,0,1,0,1.2,1.2A1.2,1.2,0,0,0,17.34,5.46Zm4.6,2.42a7.59,7.59,0,0,0-.46-2.43,4.94,4.94,0,0,0-1.16-1.77,4.7,4.7,0,0,0-1.77-1.15,7.3,7.3,0,0,0-2.43-.47C15.06,2,14.72,2,12,2s-3.06,0-4.12.06a7.3,7.3,0,0,0-2.43.47A4.78,4.78,0,0,0,3.68,3.68,4.7,4.7,0,0,0,2.53,5.45a7.3,7.3,0,0,0-.47,2.43C2,8.94,2,9.28,2,12s0,3.06.06,4.12a7.3,7.3,0,0,0,.47,2.43,4.7,4.7,0,0,0,1.15,1.77,4.78,4.78,0,0,0,1.77,1.15,7.3,7.3,0,0,0,2.43.47C8.94,22,9.28,22,12,22s3.06,0,4.12-.06a7.3,7.3,0,0,0,2.43-.47,4.7,4.7,0,0,0,1.77-1.15,4.85,4.85,0,0,0,1.16-1.77,7.59,7.59,0,0,0,.46-2.43c0-1.06.06-1.4.06-4.12S22,8.94,21.94,7.88ZM20.14,16a5.61,5.61,0,0,1-.34,1.86,3.06,3.06,0,0,1-.75,1.15,3.19,3.19,0,0,1-1.15.75,5.61,5.61,0,0,1-1.86.34c-1,.05-1.37.06-4,.06s-3,0-4-.06A5.73,5.73,0,0,1,6.1,19.8,3.27,3.27,0,0,1,5,19.05a3,3,0,0,1-.74-1.15A5.54,5.54,0,0,1,3.86,16c0-1-.06-1.37-.06-4s0-3,.06-4A5.54,5.54,0,0,1,4.21,6.1,3,3,0,0,1,5,5,3.14,3.14,0,0,1,6.1,4.2,5.73,5.73,0,0,1,8,3.86c1,0,1.37-.06,4-.06s3,0,4,.06a5.61,5.61,0,0,1,1.86.34A3.06,3.06,0,0,1,19.05,5,3.06,3.06,0,0,1,19.8,6.1,5.61,5.61,0,0,1,20.14,8c.05,1,.06,1.37.06,4S20.19,15,20.14,16ZM12,6.87A5.13,5.13,0,1,0,17.14,12,5.12,5.12,0,0,0,12,6.87Zm0,8.46A3.33,3.33,0,1,1,15.33,12,3.33,3.33,0,0,1,12,15.33Z"/>
+              </svg>
+            </v-btn>
           </v-col>
         </v-row>
       </v-container>
