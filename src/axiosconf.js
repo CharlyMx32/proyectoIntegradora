cd2a56cd799415d2c748b8cb34afaa7d837204d4
CharlyMx32 <102246854+CharlyMx32@users.noreply.github.com>
2024-08-16 15:59:52 -0600

    Update axiosconf.js

--- src/axiosconf.js
+++ src/axiosconf.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://3.135.198.47/',
+  baseURL: 'http://18.223.212.207/',
   headers: {
     'Content-Type': 'application/json'
   }
